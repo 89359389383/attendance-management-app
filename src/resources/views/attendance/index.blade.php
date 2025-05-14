639ef4b43e91c6d97a2d1c8b9f00ac69c7fbb546
Naoyuki Koyanagi <nba20093089@gmail.com>
2025-05-14 21:16:15 +0900

    resources\views\attendance\index.blade.phpタイトル変更

--- src/resources/views/attendance/index.blade.php
+++ src/resources/views/attendance/index.blade.php
@@ -1,6 +1,6 @@
 @extends('layouts.app')
 
-@section('title', 'COACHTECH勤怠管理 - 勤怠管理システム')
+@section('title', 'COACHTECH勤怠管理 - 勤怠一覧')
 
 @section('css')
 <link rel="stylesheet" href="{{ asset('css/attendance/index.css') }}">
